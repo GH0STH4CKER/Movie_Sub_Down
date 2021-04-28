97845f3bfa97ef7a84d961d657087167835d16a8
Dimuth De Zoysa <dimuthsakya@protonmail.com>
2021-04-28 19:17:58 +0530

    Add files via upload

---
+++ Movie_Sub_Down_RUN .py
@@ -0,0 +1,40 @@
+from colorama import Fore , init
+from os import system, name
+import os
+init()
+
+def clear() :
+    if name == 'nt':
+        os.system('cls')
+    else :
+        os.system('clear')
+
+lG = Fore.LIGHTGREEN_EX
+lR = Fore.LIGHTRED_EX
+lY = Fore.LIGHTYELLOW_EX
+lC = Fore.LIGHTCYAN_EX
+
+banner = """
+  █▀▄▀█ █▀█ █ █ █ █▀▀ ▄▄ █▀ █ █ █▄▄ ▄▄ █▀▄ █▀█ █ █ █ █▄ █
+  █ ▀ █ █▄█ ▀▄▀ █ ██▄    ▄█ █▄█ █▄█    █▄▀ █▄█ ▀▄▀▄▀ █ ▀█ V1.0"""
+tag = """
+ ______________________________________________________________
+                    [+] Made by GH0STH4CK3R         
+ --------------------------------------------------------------"""
+print(lG+banner,lY+tag)
+
+print(lG+"")
+choice = int(input("Website you want to download subtitles ? \n\n[1] Baiscopelk.com\n[2] Zoom.lk \n\nChoose >> "))
+
+if choice == 1 :
+    clear()
+    import Movie_Sub_Down_Baiscope
+elif choice == 2 :
+    clear()
+    import Movie_Sub_Down_Zoom
+else :
+    print(lR+"Wrong choice !")
+
+
+
+
